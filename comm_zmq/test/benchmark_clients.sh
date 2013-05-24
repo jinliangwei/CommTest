4b081b79045edff143f596ac336dca7d271cc0b9
jinliangwei <jinliangwei1@gmail.com>
2013-05-24 06:54:24 -0400

    fixed benchmark_clients.sh

--- comm_zmq/test/benchmark_clients.sh
+++ comm_zmq/test/benchmark_clients.sh
@@ -4,7 +4,7 @@
 
 if [ -z $1 ]
 then
-    echo "usage: $0 <num-clients> <sip> <sport>"
+    echo "usage: $0 <num-clients> <idstart> <sip> <sport>"
     exit
 fi
 clicnt=0
@@ -12,5 +12,6 @@ clicnt=0
 while [ $clicnt -lt $1 ]
 do
     ((clicnt += 1))
-    ../bin/benclient --id $clicnt --sip $2 --sport $3 &
+    ((id = clicnt + $2))
+    ../bin/benclient --id $id --sip $3 --sport $4 &
 done
